3d18a6c7b97bf9f005b04caf818809a3078612c1
devikasri123 <30549765+devikasri123@users.noreply.github.com>
2017-08-08 15:59:51 +0530

    positive.C

---
+++ positive.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+int n;
+printf("enter the number");
+scanf("%d",&a);
+if(n>=1)
+printf("positive numbar");
+else if(n==0)
+printf("neither positive nor negative");
+else
+printf("negative numbar");
+return 0;
+}
